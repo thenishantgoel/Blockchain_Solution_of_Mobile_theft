9a5dbe0b59b6cdc8d7b3a793c963a7d0ba696700
Nishant Goel <nishantgoel0413@gmail.com>
2024-07-08 18:17:14 +0530

    on ENTER get details

--- scripts.js
+++ scripts.js
@@ -149,6 +149,15 @@ async function initialize() {
 
       loadIMEIsFromLocalStorage();
       displayReportedIMEIs();
+      const imeiInput = document.getElementById('imeiInput');
+        if (imeiInput) {
+          imeiInput.addEventListener('keypress', (event) => {
+            if (event.key === 'Enter') {
+              event.preventDefault(); // Prevent form submission or any default behavior
+              getLostDeviceDetails(); // Call the function to report the lost device
+            }
+          });
+        }
     } catch (error) {
       console.error("User denied account access:", error);
     }
